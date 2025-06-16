cc37848752080c1a9e427cb69d60dcab2b7368ca
J2Commerce <olivier@j2commerce.com>
2025-06-16 19:49:31 -0400

    Use 'CHANGE' instead (#227)
    Co-authored-by: Olivier Buisard <olivier.buisard@simplifyyourweb.com>

--- administrator/components/com_j2store/sql/updates/mysql/4.1.0-2025-06-13.sql
+++ administrator/components/com_j2store/sql/updates/mysql/4.1.0-2025-06-13.sql
@@ -1,76 +1,76 @@
 ALTER TABLE `#__j2store_orderitems`
-    MODIFY `orderitem_per_item_tax` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `orderitem_tax` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `orderitem_discount` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `orderitem_discount_tax` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `orderitem_price` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `orderitem_option_price` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `orderitem_finalprice` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `orderitem_finalprice_with_tax` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `orderitem_finalprice_without_tax` DECIMAL(15,5) NULL DEFAULT NULL /** CAN FAIL **/;
+    CHANGE `orderitem_per_item_tax` `orderitem_per_item_tax` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `orderitem_tax` `orderitem_tax` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `orderitem_discount` `orderitem_discount` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `orderitem_discount_tax` `orderitem_discount_tax` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `orderitem_price` `orderitem_price` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `orderitem_option_price` `orderitem_option_price` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `orderitem_finalprice` `orderitem_finalprice` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `orderitem_finalprice_with_tax` `orderitem_finalprice_with_tax` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `orderitem_finalprice_without_tax` `orderitem_finalprice_without_tax` DECIMAL(15,5) NULL DEFAULT NULL /** CAN FAIL **/;
 
 ALTER TABLE `#__j2store_ordertaxes`
-    MODIFY `ordertax_percent` DECIMAL(15,5) NULL DEFAULT 0.0,
-    MODIFY `ordertax_amount` DECIMAL(15,5) NULL DEFAULT 0.0 /** CAN FAIL **/;
+    CHANGE `ordertax_percent` `ordertax_percent` DECIMAL(15,5) NULL DEFAULT 0.0,
+    CHANGE `ordertax_amount` `ordertax_amount` DECIMAL(15,5) NULL DEFAULT 0.0 /** CAN FAIL **/;
 
 ALTER TABLE `#__j2store_orderdiscounts`
-    MODIFY `discount_amount` DECIMAL(15,5) NULL DEFAULT 0.0,
-    MODIFY `discount_tax` DECIMAL(15,5) NULL DEFAULT 0.0 /** CAN FAIL **/;
+    CHANGE `discount_amount` `discount_amount` DECIMAL(15,5) NULL DEFAULT 0.0,
+    CHANGE `discount_tax` `discount_tax` DECIMAL(15,5) NULL DEFAULT 0.0 /** CAN FAIL **/;
 
 ALTER TABLE `#__j2store_orderfees`
-    MODIFY `amount` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `tax` DECIMAL(15,5) NULL DEFAULT NULL /** CAN FAIL **/;
+    CHANGE `amount` `amount` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `tax` `tax` DECIMAL(15,5) NULL DEFAULT NULL /** CAN FAIL **/;
 
 ALTER TABLE `#__j2store_orderitemattributes`
-    MODIFY `orderitemattribute_price` DECIMAL(15,5) NULL DEFAULT 0.0 /** CAN FAIL **/;
+    CHANGE `orderitemattribute_price` `orderitemattribute_price` DECIMAL(15,5) NULL DEFAULT 0.0 /** CAN FAIL **/;
 
 ALTER TABLE `#__j2store_orders`
-    MODIFY `order_total` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `order_subtotal` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `order_subtotal_ex_tax` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `order_tax` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `order_shipping` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `order_shipping_tax` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `order_discount` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `order_discount_tax` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `order_credit` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `order_refund` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `order_surcharge` DECIMAL(15,5) NULL DEFAULT NULL,
-    MODIFY `order_fees` DECIMAL(15,5) NULL DEFAULT NULL /** CAN FAIL **/;
+    CHANGE `order_total` `order_total` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `order_subtotal` `order_subtotal` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `order_subtotal_ex_tax` `order_subtotal_ex_tax` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `order_tax` `order_tax` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `order_shipping` `order_shipping` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `order_shipping_tax` `order_shipping_tax` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `order_discount` `order_discount` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `order_discount_tax` `order_discount_tax` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `order_credit` `order_credit` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `order_refund` `order_refund` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `order_surcharge` `order_surcharge` DECIMAL(15,5) NULL DEFAULT NULL,
+    CHANGE `order_fees` `order_fees` DECIMAL(15,5) NULL DEFAULT NULL /** CAN FAIL **/;
 
 ALTER TABLE `#__j2store_productprice_index`
-    MODIFY `min_price` DECIMAL(15,5) NOT NULL,
-    MODIFY `max_price` DECIMAL(15,5) NOT NULL /** CAN FAIL **/;
+    CHANGE `min_price` `min_price` DECIMAL(15,5) NOT NULL,
+    CHANGE `max_price` `max_price` DECIMAL(15,5) NOT NULL /** CAN FAIL **/;
 
 ALTER TABLE `#__j2store_shippingrates`
-    MODIFY `shipping_rate_price` DECIMAL(15,5) NULL DEFAULT 0.0,
-    MODIFY `shipping_rate_handling` DECIMAL(15,5) NULL DEFAULT 0.0 /** CAN FAIL **/;
+    CHANGE `shipping_rate_price` `shipping_rate_price` DECIMAL(15,5) NULL DEFAULT 0.0,
+    CHANGE `shipping_rate_handling` `shipping_rate_handling` DECIMAL(15,5) NULL DEFAULT 0.0 /** CAN FAIL **/;
 
 ALTER TABLE `#__j2store_customfields`
-    MODIFY `enabled` INT NULL DEFAULT 1,
-    MODIFY `ordering` INT NULL DEFAULT 0 /** CAN FAIL **/;
+    CHANGE `enabled` `enabled` INT NULL DEFAULT 1,
+    CHANGE `ordering` `ordering` INT NULL DEFAULT 0 /** CAN FAIL **/;
 
 ALTER TABLE `#__j2store_filtergroups`
-    MODIFY `enabled` INT NULL DEFAULT 1,
-    MODIFY `ordering` INT NULL DEFAULT 0 /** CAN FAIL **/;
+    CHANGE `enabled` `enabled` INT NULL DEFAULT 1,
+    CHANGE `ordering` `ordering` INT NULL DEFAULT 0 /** CAN FAIL **/;
 
 ALTER TABLE `#__j2store_lengths`
-    MODIFY `enabled` INT NULL DEFAULT 1,
-    MODIFY `ordering` INT NULL DEFAULT 0 /** CAN FAIL **/;
+    CHANGE `enabled` `enabled` INT NULL DEFAULT 1,
+    CHANGE `ordering` `ordering` INT NULL DEFAULT 0 /** CAN FAIL **/;
 
 ALTER TABLE `#__j2store_options`
-    MODIFY `enabled` INT NULL DEFAULT 1,
-    MODIFY `ordering` INT NULL DEFAULT 0 /** CAN FAIL **/;
+    CHANGE `enabled` `enabled` INT NULL DEFAULT 1,
+    CHANGE `ordering` `ordering` INT NULL DEFAULT 0 /** CAN FAIL **/;
 
 ALTER TABLE `#__j2store_product_options`
-    MODIFY `ordering` INT NULL DEFAULT 0 /** CAN FAIL **/;
+    CHANGE `ordering` `ordering` INT NULL DEFAULT 0 /** CAN FAIL **/;
 
 ALTER TABLE `#__j2store_weights`
-    MODIFY `enabled` INT NULL DEFAULT 1,
-    MODIFY `ordering` INT NULL DEFAULT 0 /** CAN FAIL **/;
+    CHANGE `enabled` `enabled` INT NULL DEFAULT 1,
+    CHANGE `ordering` `ordering` INT NULL DEFAULT 0 /** CAN FAIL **/;
 
 ALTER TABLE `#__j2store_geozones`
-    MODIFY `enabled` INT NULL DEFAULT 1 /** CAN FAIL **/;
+    CHANGE `enabled` `enabled` INT NULL DEFAULT 1 /** CAN FAIL **/;
 
 ALTER TABLE `#__j2store_products`
-    MODIFY `enabled` INT NULL DEFAULT 1 /** CAN FAIL **/;
+    CHANGE `enabled` `enabled` INT NULL DEFAULT 1 /** CAN FAIL **/;
